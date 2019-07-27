8835b1d94bf6e6e5862d63c61cf920dd198b16a6
Stanislav Khromov <stanislav.khromov@schibsted.se>
2019-07-27 08:47:18 +0200

    Fix some firefox edge cases

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,19 +1,19 @@
-/* Unminified bookmarklet code */
+javascript:
 (function () {
-    const token = '';
-    const list = 'Reading';
-    const labels = 'Foo, bar, baz bad';
+    const token = '%TOKEN%';
+    const list = '%LIST%';
+    const labels = '%LABELS%';
     const scheduleToday = true;
     const baseUrl = 'https://marvin-165117.appspot.com/api/addTask';
 
     window.history.replaceState(null, null, ' ');
 
-    const parsedList = list.trim() ? ' #' + list.replace(' ', '') : '';
+    const parsedList = list.trim() ? ' ' + decodeURIComponent('%23') + list.replace(' ', '') : '';
     const parsedLabels = labels.trim() ? ' ' + labels.split(',').map(function(label) { return '@' + label.trim().replace(' ', '') }).join(' ') : '';
     const parsedSchedule = scheduleToday ? ' +today' : '';
 
     let postData = {
-        title: document.title.replace(/[+#]/g, '') + ' - ' + window.location.href  + parsedList + parsedLabels + parsedSchedule
+        title: document.title.replace(/[+]/g, '') + ' - ' + window.location.href  + parsedList + parsedLabels + parsedSchedule
     };
 
     const response = fetch(baseUrl, {
@@ -26,11 +26,16 @@
         },
         body: JSON.stringify(postData),
     }).then(function(data) {
-        window.location.reload();
+        if (data.status >= 400 && data.status < 600) {
+            alert('Marvin is either down, or there is an issue with your Marvin token - please verify it by recreating the bookmarklet.');
+        }
+        else {
+            window.location.reload();
+        }
+        
         console.log(data)
     }).catch(function(error) {
         console.error(error);
         alert('Something went wrong when saving the page. Some pages disallow bookmarklets. If this error persists across many pages, try getting a new bookmarklet. The error was: ' + error);
     });
 })();
-

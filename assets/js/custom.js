26ed0cfa52b0cd7e3ac5aba0c9250815f7acf933
khromov <khromov@users.noreply.github.com>
2020-10-10 00:09:13 +0000

    deployed via Github Pages Deploy Action 🚀 for f64bea9fb8e45e9d8cd8b938f20dc8c144285802

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+var bookmarkletLinkElement = document.getElementById("marvin-bookmarklet");
+
+var replaceBookmarklet = function() {
+    var bookmarkletNewLink = bookmarkletTemplate;
+
+    bookmarkletNewLink = bookmarkletNewLink.replace('%TOKEN%', document.getElementById("marvin-token").value);
+    bookmarkletNewLink = bookmarkletNewLink.replace('%LIST%', document.getElementById("marvin-list").value);
+    bookmarkletNewLink = bookmarkletNewLink.replace('%LABELS%', document.getElementById("marvin-labels").value);
+    bookmarkletNewLink = bookmarkletNewLink.replace('%SCHEDULETODAY%', document.getElementById("marvin-scheduletoday").checked ? 'true' : '');
+    bookmarkletLinkElement.href = bookmarkletNewLink;
+
+    console.log(bookmarkletLinkElement.href);
+};
+
+document.getElementById("marvin-token").addEventListener('input', replaceBookmarklet);
+document.getElementById("marvin-list").addEventListener('input', replaceBookmarklet);
+document.getElementById("marvin-labels").addEventListener('input', replaceBookmarklet);
+document.getElementById("marvin-scheduletoday").addEventListener('input', replaceBookmarklet);
+
+document.addEventListener("DOMContentLoaded", function() {
+    replaceBookmarklet();
+});
